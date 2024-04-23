8bb7c59198181c1819f29f4fc8bd6917bf523bae
Prumme <aurelien23.p@gmail.com>
2024-04-23 16:37:38 +0200

    :sparkles: create simple games algo

---
+++ src/coinflip.ts
@@ -0,0 +1,6 @@
+export function coinFlip() {
+  if (Math.random() < 0.5) {
+    return "heads";
+  }
+  return "tails";
+}
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,55 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { compareCards, pickCard, initHigherLowerGame } from "./SimpleCard";
 
-console.log('Script started successfully');
-
-let currentPopup: any = undefined;
+console.log("Script started successfully");
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+WA.onInit()
+  .then(() => {
+    initHigherLowerGame();
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    WA.room.area.onEnter("clock").subscribe(() => {
+      //   Pick a random card
+      pickCard();
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+      WA.chat.sendChatMessage(
+        "Will the next card be higher or lower?",
+        "Dealer"
+      );
+    });
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    //  Listen to chat messages to get the user answer
+    WA.chat.onChatMessage((message) => {
+      if (message !== "higher" && message !== "lower") {
+        WA.chat.sendChatMessage("Please enter higher or lower", "Dealer");
+        return;
+      }
 
-}).catch(e => console.error(e));
+      const actualValue = WA.state.actualCard as any;
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+      pickCard();
+
+      const nextValue = WA.state.actualCard as any;
+
+      compareCards(actualValue, nextValue, message);
+    });
+
+    WA.room.area.onLeave("clock").subscribe(() => {
+      WA.chat.close();
+    });
+
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API  Extra ready");
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
+
+
 
 export {};
