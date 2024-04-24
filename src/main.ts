682b19c823e57033c929576197da235272685dfe
Ilyam Dupuis <ilyamdupuis0903@gmail.com>
2024-04-24 16:36:41 +0200

    feat: remove coins when player get them

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,10 @@ WA.onInit().then(() => {
     // Init the coin counter of the player
     WA.player.state.coins = 50;
     let coinSound = WA.sound.loadSound("../public/sounds/coin-sound.mp3");
+    let soundConfig = {
+        volume: 0.5,
+        loop: false,
+    };
 
     WA.ui.website.open({
         url: "./src/hud/inventory.html",
@@ -29,10 +33,47 @@ WA.onInit().then(() => {
         allowApi: true,
     });
 
-    
+    // Hiding coins 0
     WA.room.area.onEnter('add-coin').subscribe(() => {
-        coinSound.play();
-        WA.player.state.coins += 1;
+        coinSound.play(soundConfig);
+        (WA.player.state.coins as number) += 10;
+        WA.room.area.delete('add-coin');
+        WA.room.hideLayer('floor/coins/coin-0');
+    });
+    // Hiding coins 1
+    WA.room.area.onEnter('add-coin-1').subscribe(() => {
+        coinSound.play(soundConfig);
+       (WA.player.state.coins as number) += 10;
+        WA.room.area.delete('add-coin-1');
+        WA.room.hideLayer('floor/coins/coin-1');
+    });
+    // Hiding coins 2
+    WA.room.area.onEnter('add-coin-2').subscribe(() => {
+        coinSound.play(soundConfig);
+        (WA.player.state.coins as number) += 5;
+        WA.room.area.delete('add-coin-2');
+        WA.room.hideLayer('floor/coins/coin-2');
+    });
+    // Hiding coins 3
+    WA.room.area.onEnter('add-coin-3').subscribe(() => {
+        coinSound.play(soundConfig);
+       (WA.player.state.coins as number) += 5;
+        WA.room.area.delete('add-coin-3');
+        WA.room.hideLayer('floor/coins/coin-3');
+    });
+    // Hiding coins 4
+    WA.room.area.onEnter('add-coin-4').subscribe(() => {
+        coinSound.play(soundConfig);
+       (WA.player.state.coins as number) += 10;
+        WA.room.area.delete('add-coin-4');
+        WA.room.hideLayer('floor/coins/coin-4');
+    });
+    // Hiding coins 5
+    WA.room.area.onEnter('add-coin-5').subscribe(() => {
+        coinSound.play(soundConfig);
+       (WA.player.state.coins as number) += 10;
+        WA.room.area.delete('add-coin-5');
+        WA.room.hideLayer('floor/coins/coin-5');
     });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
